ba180da57adbc1087fd00482e6aa1be324b37f89
Mallorie Surrrency <mallorieblair@gmail.com>
2022-01-17 02:21:45 -0500

    Installed express and middleware - set up the server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 // create an api router
 // attach other routers from files in this api directory (users, activities...)
 // export the api router
+const express = require('express');
+const apiRouter = express.Router();
 
+
+
+module.exports = apiRouter;
 
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // create the express server here
+require('dotenv').config();
+const { PORT = 3000 } = process.env
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const { client } = require('./db/client');
+server.use(morgan('dev'));
+server.use(cors());
+
+server.use(express.json())
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+
+client.connect();
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});

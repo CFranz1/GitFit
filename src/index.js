b8b4e7fa8f018aa0a678b62bb77ee82c3e5952a0
Cfranz1 <cfranz495@gmail.com>
2022-01-20 12:53:11 -0600

    folder setup kind of

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+
+
+
+function App(){
+
+    
+    return(
+        <div>its ya app doing its thang</div>
+
+    )
+   
+}
+const app = document.getElementById('app')
+ReactDOM.render(<App />, app)

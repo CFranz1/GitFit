dfddc166ba75ef18dcb28b5e8978e537f04665a9
Cfranz1 <cfranz495@gmail.com>
2022-03-04 13:49:10 -0600

    heroku ready commit 2

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 // build and export your unconnected client here
 const { Client } = require('pg'); // imports the pg module
-const client = new Client('postgres://localhost:5432/fitness-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/phenomena-dev');
 
 module.exports={
     client

2e410f4d3bff07474258fec1cbf705f4b60091f0
Cfranz1 <cfranz495@gmail.com>
2022-03-09 19:07:48 -0600

    heroku ready deploy

--- index.js
+++ index.js
@@ -9,41 +9,38 @@ const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
-const bodyParser = require('body-parser')
-const cors = require('cors');
-const { client } = require('./db/client');
+server.use(express.json())
 
-server.use(cors());
+
 
 const path = require("path");
 server.use(express.static(path.join(__dirname, "build")));
 
-
-
-client.connect();
-server.use(express.json())
-
-server.use(bodyParser.json())
-server.use((req, res, next) => {
-  console.log("Body is now", req.body);
-  next();
-});
-
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
+
 
 server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+const { client } = require('./db/client');
+
 server.use((err, req, res, next) => {
   //console.log(error)
   res.status(500).send({ 'error': err.stack });
-
-
 });
+
 
 
 server.listen(PORT, async () => {
   console.log('The server is up on port', PORT)
+
+
+  try {
+    await client.connect();
+    console.log("Database is open for business!");
+  } catch (error) {
+    console.error("Database is closed for repairs!\n", error);
+  }
 });

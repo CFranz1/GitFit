db1973d497711dd429087b37fd9fa89b10f725ff
Cfranz1 <cfranz495@gmail.com>
2022-03-09 19:29:22 -0600

    heroku ready deploy

--- db/client.js
+++ db/client.js
@@ -2,4 +2,4 @@
 const { Client } = require('pg'); // imports the pg module
 const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/phenomena-dev');
 
-module.exports = client;
+module.exports = { client };
